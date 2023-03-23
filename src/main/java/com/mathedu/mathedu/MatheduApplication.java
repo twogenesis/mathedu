e80b5d583dfe5080b8defcbe0ea941e72fbf2dcc
yhKim <twogenesis@naver.com>
2023-03-23 13:19:14 +0900

    First project file commit

---
+++ src/main/java/com/mathedu/mathedu/MatheduApplication.java
@@ -0,0 +1,13 @@
+package com.mathedu.mathedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatheduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathedu/mathedu/MatheduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathedu.mathedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
